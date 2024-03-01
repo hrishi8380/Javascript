060bc2a023e91b776a9b8750a67f04cbf18b9610
hrishi8380 <92716782+hrishi8380@users.noreply.github.com>
2024-03-01 20:39:52 +0000

    varibles

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234321
+let accountEmail = "hrishi007@gmail.com"
+var accountPassword = "Hrushi@123"
+accountCity = "pune"
+
+//accountId = 22 // not allowed
+
+accountEmail = "mahesh1212@gmail.com"
+accountPassword ="Mahesh@123"
+accountCity = "parbhani"
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
