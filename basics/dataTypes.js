7dd767a0fa7bdbc3f7425fbe01257b90f7205e4d
hrishi8380 <92716782+hrishi8380@users.noreply.github.com>
2024-03-01 21:16:21 +0000

    Data types

---
+++ basics/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+ // alert(3+3)// we are using nodeJs, not browser
+
+ console.log(3+3)
+ console.log("Hrushikesh")
+ //code readability should be high
+
+ //primtive datatypes:
+
+ //number => 2 to the power 52
+ //bigint
+ //string => ""
+ //boolean=> True/False
+ //null=> standalone value its a object
+ //undefine=>
+ //symbol=> unique
+
+ console.log(typeof null) // object
+ console.log(typeof undefined)// undefine
+ 
