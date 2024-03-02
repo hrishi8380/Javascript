954dc99cd86a0acecf6c11110e8d330c601c07d7
hrishi8380 <92716782+hrishi8380@users.noreply.github.com>
2024-03-02 20:28:35 +0000

    operations

---
+++ basics/conversionOprations.js
@@ -0,0 +1,15 @@
+let score = undefined
+
+//console.log(typeof score);
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33"=> 33
+//"33abc"=> NaN
+// true=> 1 , false=>
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
